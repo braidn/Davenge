768cb10521972a2147fb7f0437a99ff6e139a8db
Braden Douglass <ping@bradendouglass.com>
2011-10-29 17:11:45 -0400

    added a complete book model and migration

--- db/migrate/20111029205600_create_books.rb
+++ db/migrate/20111029205600_create_books.rb
@@ -1,6 +1,24 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.date :published_at
+      t.date :copyright
+      t.test :description
+      t.string :page_count
+      t.string :publisher
+      t.string :designer
+      t.string :editor
+      t.string :technical_editor
+      t.string :copyeditor
+      t.string :isbn_10
+      t.string :isbn_13
+      t.string :language
+      t.string :format
+      t.integer :file_size
+      t.string :product_dimensions
+      t.string :shipping_weight
 
       t.timestamps
     end
