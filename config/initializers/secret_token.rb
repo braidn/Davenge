d752ea5536a2fd125bba8a37ca10893e777136ba
Braden Douglass <ping@bradendouglass.com>
2011-10-29 15:57:54 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DavengeLib::Application.config.secret_token = '33cb99add314fed6c233e2703b3fdd4947e8aa6500bcb29e0b766ed4538740e649e1d085c2a4f63c65b9002d9438f3bc8ce08b56281270302e88736eb81a8597'
