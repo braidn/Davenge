cfb5e91c8c2138b43363515a63a85e288ad07ddc
Braden <braden.douglass@gmail.com>
2012-01-29 22:34:33 -0500

    added a simple login root home and removed the public index file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!
   def index
+    if user_signed_in?
+      flash.now[:notice] = 'Welcome back'
+    else
+      flash[:alert] = 'Please feel free to log your ass in'
+      redirect_to(new_user_registration_path)
+    end
   end
 end
